041ec36df5f86770371a6aa8d06aa0bc13dab85d
Jarrett <j287z337@ku.edu>
2020-10-17 13:58:15 -0500

    The ship will now move once either to the right or left, afterwards it resets to the default middle position, I cannot figure out how to fix this

--- display.py
+++ display.py
@@ -5,7 +5,6 @@ import math
 class Display:
 
     def __init__ (self):
-
         pygame.init()
         global screen
         screen = pygame.display.set_mode((1280,981))
@@ -15,35 +14,37 @@ class Display:
         pygame.display.set_caption("Space Invaders!")
         screen.blit(background,(0,0))
 
+class Player:
 
-    def player(self,x,y):
-
-        GREEN = (54,223,42)
+    def __init__(self,xpos,ypos):
+        global x
+        x = 640
+        global y 
+        y = 890
         self.x = x
         self.y = y
+        GREEN = (54,223,42)
         global playerimg
         playerimg = pygame.Rect(x,y,30,30)
         pygame.draw.rect(screen,GREEN,playerimg,0) #current player is a square, add graphics later on
 
-
-    def player_move(self,x,y):
+    def player_action(self,x,y):
         self.x = x
         self.y = y
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x -= 15
-                print("left")
-            if event.key == pygame.K_RIGHT:
-                x += 15
-                print("right")
-        playerimg.move_ip(x,y)
-        pygame.display.flip()
-        pygame.display.update()
-
+        key_press = pygame.key.get_pressed()
+        if key_press[pygame.K_LEFT]:
+            x -= 15
+        if key_press[pygame.K_RIGHT]:
+            x += 15
+        playerimg = pygame.Rect(x,y,30,30) #reestablish the rectangle with the next x coordinate
+        screen.blit(background,(0,0)) #redraws the background
+        pygame.draw.rect(screen,(52,223,42),playerimg,0) #redraws the square with its new position
+        pygame.display.flip() #updates the screen with all changes
 
 
 mygame = Display()
-mygame.player(640,890)
+playership = Player(640,890)
+
 running = True
 
 while running:
@@ -52,7 +53,9 @@ while running:
     pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            mygame.player_move(640,890)
+            playership.player_action(x,y)
+
+
 
         if event.type == pygame.QUIT:
             pygame.quit()
