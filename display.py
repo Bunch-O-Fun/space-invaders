a5590efe8666f5d80d20f37ca85b3b7603acf05b
Jarrett <j287z337@ku.edu>
2020-10-17 08:31:54 -0500

    Got the display made, added a space background aswell

--- display.py
+++ display.py
@@ -4,5 +4,23 @@ import math
 
 class Display:
 
+    def __init__ (self):
+
+        screen = pygame.display.set_mode((1280,981))
+        background = pygame.image.load('background.png')
+        pygame.display.set_caption("Space Invaders!")
+        screen.blit(background,(0,0))
+
 
 mygame = Display()
+running = True
+
+while running:
+    pygame.display.flip() #updates the visuals on the screen
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False #breaks out of loop and quits the game
+            sys.exit()
+
+pygame.quit()
