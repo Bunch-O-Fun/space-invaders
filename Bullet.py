712a25905bab1c429f74808d3ed93c501ee62b81
Turner Graham <t467g332@ku.edu>
2020-10-20 15:27:53 -0500

    Modifications to Bullet class

--- Bullet.py
+++ Bullet.py
@@ -1,17 +1,10 @@
 class Bullet:
 
-    __position__ = [0,0] # bullet keeps track of its location via a 2D array (x,y)
-    __speed__ = 0 # bullet's speed is a integer value
+    __position__ = [0,0] # bullet keeps track of its location via a 2 element array (x,y)
 
-    def __init__(self, x, y, speed): # initializer for the bullet, allowing specification of its starting location and its speed
+    def __init__(self, x, y): # initializer for the bullet, allowing specification of its starting location and its speed
         self.__position__[0]= x
         self.__position__[1] = y
-        self.__speed__ = speed
-
-    def setSpeed(self, speed): # sets the speed of the bullet, if needed
-        self.__speed__ = speed
-    def getSpeed(self): # returns the speed of the bullet
-        return self.__speed__
 
     def setPosition(self, x, y): # sets the x and y coordinates of the bullet
         self.__position__[0] = x
