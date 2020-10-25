3ffcf452e209e33eb9bea31fb06cc97cfdc0d9aa
Carter Harrod <carter.harrod283@gmail.com>
2020-10-25 13:59:15 +0000

    documentation added

--- display.py
+++ display.py
@@ -3,12 +3,12 @@ import sys
 import math
 import time
 
-SCREEN_HEIGHT = 981
-SCREEN_WIDTH = 1290
-RIGHT_BORDER = SCREEN_WIDTH - 85
-LEFT_BORDER = 85
+SCREEN_HEIGHT = 981                 
+SCREEN_WIDTH = 1290 
+RIGHT_BORDER = SCREEN_WIDTH - 85        # Border is the maximum a player and alien can go
+LEFT_BORDER = 85                        # left or right on the screen
 
-ALIEN_SPEED = 10
+ALIEN_SPEED = 10                        # Number of pixels moved per frame
 BULLET_SPEED = 10
 PLAYER_SPEED = 5 
 
@@ -42,35 +42,75 @@ class Display:
         text = font.render("GAME OVER", True, (255, 255, 255))
         screen.blit(text, (400, 250))
 
+
+
 class Player:
 
+    '''
+    Player Constructor
+    Parameters: xpos, ypos (x and y starting position of player)
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: creates a valid player at xpos and ypos
+    '''
     def __init__(self,xpos,ypos):
         self.x = xpos
         self.y = ypos
        
-        self.direction = 0
+        self.direction = 0      #Direction player moves, 1 = right, -1 = left, 0 is dont move
         self.hitbox = pygame.Rect(self.x,self.y,30,30)
         #current player is a square, add graphics later on
     
+    '''
+    move
+    Parameters: N/A
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: moves player an amount of pixels in a direction 
+    '''
     def move(self):
+        # if statement checks if the player is going outside the border
         if (self.x + self.direction * PLAYER_SPEED > LEFT_BORDER) and (self.x + self.direction * PLAYER_SPEED < RIGHT_BORDER):
             self.x = self.x + self.direction * PLAYER_SPEED
 
+    '''
+    render
+    Parameters: N/A
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: renders a 30 x 30 box to represent the player
+    '''
     def render(self):
+        # renders players box and saves it's hit box
         self.hitbox = pygame.Rect(self.x,self.y,30,30)
         pygame.draw.rect(screen,(54,223,42),self.hitbox,0)
 
 class Alien:
+
+    '''
+    alien constructor
+    Parameters: x and y position, as well as the size in pixels of the alien
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: creates an alien
+    '''
     def __init__(self,xpos,ypos, xsize, ysize):
         self.x = xpos
         self.y = ypos
         self.yold = ypos
-        self.direction = 1
+        self.direction = 1 # 1 = right, -1 = left, 2 & -2 = down
         self.xsize = xsize
         self.ysize = ysize
         self.hitbox = pygame.Rect(self.x,self.y,self.xsize,self.ysize)
         #current player is a square, add graphics later on
     
+    '''
+    move
+    Parameters: N/A
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: moves an alien in a direction
+    '''
     def move(self):
         if(abs(self.direction) != 2):
             self.x = self.x + self.direction * ALIEN_SPEED
@@ -78,29 +118,59 @@ class Alien:
             self.y += ALIEN_SPEED
 
 
-
+    '''
+    render
+    Parameters: N/A
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: updates hitbox and draws self on screen
+    '''
     def render(self):
         self.hitbox = pygame.Rect(self.x,self.y,self.xsize,self.ysize)
         pygame.draw.rect(screen,(54,223,42),self.hitbox,0)
 
-    def getX(self):
-        return self.x
-    def getY(self):
-        return self.y
-
 class Bullet:
+    '''
+    bullet constructor
+    Parameters: x and y position
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: bullet created at location
+    '''
     def __init__(self, x, y): # initializer for the bullet, allowing specification of its starting location and its speed
         self.x = x
         self.y = y
         self.hitbox = pygame.Rect(self.x,self.y,5,10)
 
+    '''
+    move 
+    Parameters: N/A
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: bullet moved upwards
+    '''
     def move(self):
         self.y -= BULLET_SPEED
 
+    '''
+    render
+    Parameters: N/A
+    Returns: N/A
+    Preconditions: N/A
+    Postconditions: updates hitbox and draws self on screen
+    '''
     def render(self): # this works :)
         self.hitbox = pygame.Rect(self.x,self.y,5,10)
         pygame.draw.rect(screen,(255,0,0),self.hitbox,0)
 
+
+'''
+move list of bullets
+Parameters: list of bullets you want to move
+Returns: N/A
+Preconditions: N/A
+Postconditions: if bullet exits screen, removes that bullet from the list
+'''
 def moveBullets(bullets):
     for bullet in bullets:
         bullet.move()
@@ -109,30 +179,16 @@ def moveBullets(bullets):
         if bullets[0].y < -10:
             bullets.remove(bullets[0])
 
-def getUserInput(playership, bullets):
-    for event in pygame.event.get():
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playership.direction = -1
-            if event.key == pygame.K_RIGHT:
-                playership.direction = 1
-        
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                playership.direction = 0
-            if event.key == pygame.K_RIGHT:
-                playership.direction = 0
-            
-            if event.key == pygame.K_SPACE:
-                bullets += [Bullet(playership.x + 12, playership.y)]
-                        
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            running = False #breaks out of loop and quits the game
-            sys.exit()
-
+'''
+move list of aliens
+Parameters: list of aliens you want to move
+Returns: N/A
+Preconditions: N/A
+Postconditions: if aliens exit screen, ends game
+'''
 def moveAliens(aliens):
+
+    # checks if the aliens hit a border, if they do turns them around to go the other way (the aliens move down a bit and then change direction)
     if(len(aliens) != 0):
         if(aliens[len(aliens) - 1].x >= RIGHT_BORDER and aliens[len(aliens) - 1].direction == 1):
             for alien in aliens:
@@ -150,11 +206,55 @@ def moveAliens(aliens):
                 for alien in aliens:
                     alien.direction = 1
                     alien.yold = alien.y
+
+    # moves all the aliens and checks if they leave screen
     for alien in aliens:
         alien.move()
         if(alien.y > SCREEN_HEIGHT):
             running = False
 
+'''
+gets keypresses like arrow keys and spacebar
+Parameters: playership and list of the player's bullets
+Returns: N/A
+Preconditions: N/A
+Postconditions: user input is updated and executed
+'''
+def getUserInput(playership, bullets):
+    for event in pygame.event.get():
+
+        # on key pressed, change player direction to arrow key direction
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playership.direction = -1
+            if event.key == pygame.K_RIGHT:
+                playership.direction = 1
+
+            # create a bullet if player presses space
+            if event.key == pygame.K_SPACE:
+                bullets += [Bullet(playership.x + 12, playership.y)]
+        
+        # on key released change player direction to stop moving
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                playership.direction = 0
+            if event.key == pygame.K_RIGHT:
+                playership.direction = 0
+            
+            
+                        
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False #breaks out of loop and quits the game
+            sys.exit()
+
+'''
+checks if bullet overlaps with alien
+Parameters: list of bullets and aliens
+Returns: N/A
+Preconditions: N/A
+Postconditions: if a alien and bullet overlap deletes them
+'''
 def checkHit(bullets, aliens):
     for alien in aliens:
         for bullet in bullets:
@@ -162,6 +262,13 @@ def checkHit(bullets, aliens):
                 aliens.remove(alien)
                 bullets.remove(bullet)
 
+'''
+increments difficulty if all aliens are killed
+Parameters: N/A
+Returns: N/A
+Preconditions: N/A
+Postconditions: if all aliens dead makes difficulty harder
+'''
 def levelUP(): #Goes to next level if player kills all aliens on current level
     global level, ALIEN_SPEED
     next_level = True
@@ -178,6 +285,13 @@ def levelUP(): #Goes to next level if player kills all aliens on current level
                 aliens[i] += [Alien(100 + j * 60, 40 + 40 * i, 35, 20)]
     return level
 
+'''
+checks end condition of game (aliens leave the board)
+Parameters: N/A
+Returns: N/A
+Preconditions: N/A
+Postconditions: if end condition exits game
+'''
 def checkEnd(aliens):
     global running
     for row_aliens in aliens:
@@ -185,6 +299,13 @@ def checkEnd(aliens):
             if(alien.y > SCREEN_HEIGHT):
                 running = False
 
+'''
+executes 1 frame of game logic
+Parameters: N/A
+Returns: N/A
+Preconditions: N/A
+Postconditions: game is updated to next frame
+'''
 def gameLogic(playership, bullets, aliens):
     getUserInput(playership, bullets)
     playership.move() #moves player in direction, self.direction updated in playerInput
@@ -198,8 +319,13 @@ def gameLogic(playership, bullets, aliens):
     levelUP()
     checkEnd(aliens)
     
-    
-    
+'''
+renders 1 frame
+Parameters: N/A
+Returns: N/A
+Preconditions: N/A
+Postconditions: fram is rendered and ready to flip
+'''
 def render(playership, bullets, aliens):
     screen.blit(background,(0,0))
     playership.render()
@@ -210,7 +336,6 @@ def render(playership, bullets, aliens):
             alien.render()
      #updates the visuals on the screen
     #pygame.display.update()
-
 
 mygame = Display()
 x = 640
@@ -229,7 +354,7 @@ while running:
     gameLogic(playership, bullets, aliens)
     
     time_end = pygame.time.get_ticks()
-    if(time_end - time_start < 17):
+    if(time_end - time_start < 17): # regulates framerate to 60fps
         pygame.time.delay(17 - (time_end - time_start))
     pygame.display.flip()
 
@@ -242,33 +367,3 @@ while True:
             pygame.quit()
             running = False #breaks out of loop and quits the game
             sys.exit()
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#spce
